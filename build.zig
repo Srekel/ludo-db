6fe112aca90fdc4734505d3a57bddf725692706b
Anders Elfgren <srekel@gmail.com>
2024-07-07 18:25:44 +0200

    Support for fast check-on-save.

--- build.zig
+++ build.zig
@@ -47,4 +47,37 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    //
+    // Fast "check" build
+    // https://kristoff.it/blog/improving-your-zls-experience/
+    const exe_check = b.addExecutable(.{
+        .name = "LudoDB",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    @import("system_sdk").addLibraryPathsTo(exe_check);
+
+    @import("zgpu").addLibraryPathsTo(exe_check);
+
+    exe_check.root_module.addImport("zgui", zgui.module("root"));
+    exe_check.linkLibrary(zgui.artifact("imgui"));
+
+    {
+        const zglfw = b.dependency("zglfw", .{});
+        exe_check.root_module.addImport("zglfw", zglfw.module("root"));
+        exe_check.linkLibrary(zglfw.artifact("glfw"));
+
+        const zpool = b.dependency("zpool", .{});
+        exe_check.root_module.addImport("zpool", zpool.module("root"));
+
+        const zgpu = b.dependency("zgpu", .{});
+        exe_check.root_module.addImport("zgpu", zgpu.module("root"));
+        exe_check.linkLibrary(zgpu.artifact("zdawn"));
+    }
+
+    const check = b.step("check", "Check if ludodb compiles");
+    check.dependOn(&exe_check.step);
 }
